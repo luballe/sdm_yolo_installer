c2a88f0b178feb7f167430e14fa01ae6d199ea93
Luis Ballesteros <luballe@luballe.com>
2020-06-29 11:48:43 -0500

    Update sdm_yolo_installer.sh
    adding debug sleep 10 s

--- sdm_yolo_installer.sh
+++ sdm_yolo_installer.sh
@@ -3,22 +3,24 @@
 # Tasks to do before rebooting
 before_reboot(){
 
-    echo "*** Setting up datetime.."
+    echo "*** Setting up datetime..."
     cp /usr/share/zoneinfo/America/Bogota /etc/localtime
     echo "Datetime set up!"
-
-    echo "*** Setting up installation directory..."
-    cur_dir=$(pwd)
-    echo "*** Current directory: "$cur_dir
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Update system
     echo "*** Updating system..."
     sudo apt -y update
     sudo apt -y upgrade
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Install desktop environment
     echo "*** Installing desktop environment..."
     sudo apt -y install xfce4
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Install remote desktop server
     echo "*** Installing remote desktop server..."
@@ -26,11 +28,15 @@ before_reboot(){
     sudo systemctl enable xrdp
     echo xfce4-session >~/.xsession
     sudo service xrdp restart
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Prerequisites
     echo "*** Installing gcc g++..."
     # gcc g++
     sudo apt -y install gcc g++
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Utils
     echo "*** Installing zip unzip cmake iotop keychain..."
     sudo apt -y install zip
@@ -38,23 +44,35 @@ before_reboot(){
     sudo apt -y install cmake
     sudo apt -y install iotop
     sudo apt -y install keychain
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # FFMPEG
     echo "*** Installing ffmpeg..."
     sudo apt -y install ffmpeg
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Python
     echo "*** Installing python numpy..."
     sudo apt -y install python-dev python-numpy
     sudo apt -y install python3-dev python3-numpy
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # GTK support for GUI features
     echo "*** Installing libavcodec libgstreamer..."
     sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev
     sudo apt -y install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Support for gtk2 OpenCV:
     echo "*** Installing libgtk2..."
     sudo apt -y install libgtk2.0-dev
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Support for gtk3 OpenCV:
     echo "*** Installing libgtk3..."
     sudo apt -y install libgtk-3-dev
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Optional dependencies for OpenCV
     echo "*** Installing OpenCV dependencies..."
     sudo apt -y install libpng-dev
@@ -62,18 +80,26 @@ before_reboot(){
     sudo apt -y install libopenexr-dev
     sudo apt -y install libtiff-dev
     sudo apt -y install libwebp-dev
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # TBB support for OpenCV
     echo "*** Installing TBB support for OpenCV..."
     sudo apt -y install libtbb-dev
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Install pip and tk
     echo "*** Installing pip tk..."
     sudo apt -y install python-pip
     sudo apt -y install python3-pip
     sudo apt -y install python-tk
     sudo apt -y install python3-tk 
+    echo "*** Sleeping 10 secs..."
+    sleep 10
     # Install easyGui
     echo "*** Installing Easygui..."
     sudo -H pip install easygui
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Install Nvidia Driver
     echo "*** Installing nvidia driver..."
@@ -84,13 +110,19 @@ before_reboot(){
     sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
     sudo apt -y update
     sudo apt -y install cuda
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Testing nvidia GPU..."
     nvidia-smi
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Downloading OpenCV..."
     wget https://github.com/opencv/opencv/archive/2.4.13.6.zip -O $cur_dir/opencv.zip
     unzip $cur_dir/opencv.zip
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Compiling OpenCV..."
     mkdir -p $cur_dir/opencv-2.4.13.6/build
@@ -99,10 +131,14 @@ before_reboot(){
     make -j6
     echo "*** Installing OpenCV..."
     make install
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Downloading sdm-yolo..."
     cd $cur_dir
     git clone https://github.com/calderonf/sdm_yolo.git
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Downloading weights..."
     # Download utility for downloading google drive link
@@ -110,18 +146,24 @@ before_reboot(){
     # Download zip file
     curl gdrive.sh | bash -s https://drive.google.com/file/d/1TnmqD8zmfnW0Vn9hylgEY33r4lCxU1jH
     unzip weights.zip
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     # Export Path
     echo "*** Exporting Path..."
     var='export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}'
     # | grep '\$' is for not expanding variable $var
     echo "$var" | grep '\$' >> $cur_dir/.bashrc
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
 # Tasks to do after rebooting
 after_reboot(){
     echo "*** Compiling SDM_YOLO..."
     cd $cur_dir/sdm_yolo
     make -j6
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Changing Ownership..."
     cd $cur_dir
@@ -130,12 +172,16 @@ after_reboot(){
     chown -R administrador:administrador sdm_yolo
     chown -R administrador:administrador weights
     chown -R administrador:administrador gdrive.sh
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 
     echo "*** Cleaning up..."
     rm $cur_dir/cuda.deb
     rm $cur_dir/opencv.zip
     rm $cur_dir/weights.zip
     rm $cur_dir/opencv-2.4.13.6
+    echo "*** Sleeping 10 secs..."
+    sleep 10
 }
 
 #Am I running as root?
@@ -148,24 +194,28 @@ fi
 
 echo "*** I'm root!"
 
+echo "*** Setting up installation directory..."
+cur_dir=$(pwd)
+echo "*** Current directory: "$cur_dir
+
+
 if [ ! -f /var/run/resume-after-reboot ]; then
-    echo "running script for the first time...s"
+    echo "running script for the first time..."
     before_reboot
     # Preparation for reboot
     script="bash /install_sdm_yolo.sh"
     # add this script to bashrc so it gets triggered immediately after reboot
-    echo "$script" >> ~/.bashrc 
+    echo "$script" >> $cur_dir/.bashrc 
     # create a flag file to check if we are resuming from reboot.
     touch /var/run/resume-after-reboot
     echo "rebooting in 10 secs..."
-    delay 10
+    sleep 10
     sudo reboot
 else
     echo "resuming script after reboot..."
     # Remove the line that we added in zshrc
-    sed -i '/bash/d' ~/.bashrc 
+    sed -i '/bash/d' $cur_dir/.bashrc 
     # remove the temporary file that we created to check for reboot
     rm -f /var/run/resume-after-reboot
     after_reboot
 fi
-
